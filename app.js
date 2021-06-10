fca6ff24673306997b23c483268398fa4f808217
mohak-lab <mohak143m@gmail.com>
2021-06-10 18:31:14 +0530

    website hosting

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-mohak:mohak143@dailyjournel.tggsr.mongodb.net/blogDB", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
@@ -79,6 +79,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function() {
+app.listen(port , function() {
   console.log("Server started on port 3000");
 });
